87337dfc49e3fa1e31d95a33b1a591f4eace87af
MatveiKylosov <matveikyl@mail.ru>
2024-04-25 22:52:11 +0500

    Созданный класс Config. Kylosov

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Kylosov.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=localhost;" + "uid=root;" + "pwd=root" + "database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
