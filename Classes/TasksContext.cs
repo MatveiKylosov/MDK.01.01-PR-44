7eba634eec87d623c25280cb6dbba17c9f8dec70
MatveiKylosov <matveikyl@mail.ru>
2024-04-25 23:24:17 +0500

    Разработанный контекст данных для модели задач. Kylosov

---
+++ Classes/TasksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager_Kylosov.Classes.Database;
+using TaskManager_Kylosov.Models;
+
+namespace TaskManager_Kylosov.Classes
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+        }
+    }
+}
