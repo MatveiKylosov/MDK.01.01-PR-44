6af9f8aebc7073d977bea824a967cefde67fcdd0
MatveiKylosov <matveikyl@mail.ru>
2024-05-03 01:50:49 +0500

    sql fix

--- SQL/TaskManager.sql
+++ SQL/TaskManager.sql
@@ -1,16 +1,17 @@
-CREATE TABLE `Priorities` (
-  `Name` varchar(30) NOT NULL,
-  PRIMARY KEY (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLECREATE TABLE `Priority` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `NamePriority` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `Tasks` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) DEFAULT NULL,
-  `Priority` varchar(30) DEFAULT NULL,
   `DateExecute` date DEFAULT NULL,
   `Comment` varchar(1000) DEFAULT NULL,
   `Done` tinyint(1) DEFAULT NULL,
+  `PriorityId` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  KEY `Priority` (`Priority`),
-  CONSTRAINT `Tasks_ibfk_1` FOREIGN KEY (`Priority`) REFERENCES `Priorities` (`Name`)
+  KEY `fk_Tasks_Priority` (`PriorityId`),
+  CONSTRAINT `fk_Tasks_Priority` FOREIGN KEY (`PriorityId`) REFERENCES `Priority` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
